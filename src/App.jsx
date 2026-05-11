496058e80202cea8e29237e227965dba82a19189
sahla <sahlaanasks@gmail.com>
2026-05-11 07:28:26 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "40px",
+        fontWeight: "bold",
+      }}
+    >
+      BTC CI/CD Demo
+    </div>
+  );
+}
+
+export default App;
